bb6507e831e2ff8b45f6fd492d5dc129d0af09ff
saccharin01 <chousik01@gmail.com>
2025-04-16 18:45:42 +0900

    new | page.tsx
    chart 컴포넌트를 포함하는 랜딩페이지 스켈레톤 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+"use client"
+
+import ChartComponent from "../components/chart"
+
 export default function Home(){
   return(
+    <>
     <div>
       <h1>
         Hello World!
       </h1>
     </div>
+
+    <div>
+      <ChartComponent/>
+    </div>
+    
+    </>
   )
 }
