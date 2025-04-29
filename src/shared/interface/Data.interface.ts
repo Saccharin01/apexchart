1e6f5fbbfc2c6655675054e16f7409cb51d99440
saccharin01 <chousik01@gmail.com>
2025-04-30 06:05:14 +0900

    feat | data.interface.ts
    서버에서 반환하는 데이터 형식 추가

--- src/shared/interface/Data.interface.ts
+++ src/shared/interface/Data.interface.ts
@@ -6,6 +6,19 @@ export interface ProductDTO {
   update_at : Date
 }
 
-export default interface SensorUnitIds {
+export interface SensorUnitIds {
   chipIds: string[];
+}
+
+
+export interface SensorDataDTO {
+  sensedData: number;
+  sensedTime: string; // ISO 형식의 문자열 (e.g., "2025-04-29T23:12:41")
+}
+
+export interface SensorResponseDTO {
+  chipId: string;
+  name: string;
+  location: string;
+  data: SensorDataDTO[];
 }
