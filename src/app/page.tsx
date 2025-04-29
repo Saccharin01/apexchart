3bf9240a61fad9ffbe1f6a87238c12028ac6ab00
saccharin01 <chousik01@gmail.com>
2025-04-30 04:26:06 +0900

    feat | page.tsx
    메인 페이지의 레이아웃 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
-"use client"
+"use client";
 
-import ChartComponent from "../components/chart"
+import SideMenu from "@/components/sideMenu/sideMenu";
+import ChartComponent from "../components/chart";
 
-export default function Home(){
-  return(
-    <>
-    <div>
-      <h1>
-        Hello World!
-      </h1>
+export default function Home() {
+  return (
+    <div className="flex min-h-screen">
+      <SideMenu />
+      <main className="flex-1 ml-0 md:p-6 bg-gray-50">
+        <h1 className="text-3xl font-bold mb-6">Hello World!</h1>
+        <ChartComponent />
+      </main>
     </div>
-
-    <div>
-      <ChartComponent/>
-    </div>
-    
-    </>
-  )
+  );
 }
