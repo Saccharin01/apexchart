3e28b5182f2c137e85094c178f32a9207381776d
saccharin01 <chousik01@gmail.com>
2025-04-30 06:29:11 +0900

    feat | page.tsx
    컨텍스트 수정 및 name, location 프로퍼티를 이용해 dom 구조 랜더링. 단 해당 값이 없을 경우 삼항 연산을 통해 예외처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,23 @@
 
 import SideMenu from "@/components/sideMenu/sideMenu";
 import ChartComponent from "../components/chart";
+import { useChartData } from "@/app/hooks/ChartDataContext"; // 컨텍스트 훅 가져오기
 
 export default function Home() {
+  const { sensorData } = useChartData();
+
   return (
     <div className="flex min-h-screen">
       <SideMenu />
       <main className="flex-1 ml-0 md:p-6 bg-gray-50">
-        <h1 className="text-3xl font-bold mb-6">Hello World!</h1>
+        <h1 className="text-3xl font-bold mb-2">
+          {sensorData?.name ? sensorData.name : "Hello World!"}
+        </h1>
+
+        {sensorData?.location && (
+          <p className="text-gray-500 mb-6">📍 {sensorData.location}</p>
+        )}
+
         <div className="w-full h-96 bg-gray-100 rounded-lg shadow p-4">
           <ChartComponent />
         </div>
