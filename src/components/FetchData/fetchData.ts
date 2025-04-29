288967104af845f471fb3cf9cfe5e5c880c9da62
saccharin01 <chousik01@gmail.com>
2025-04-30 04:23:59 +0900

    feat | fetchData.ts
    데이터를 페칭하는 헬퍼 함수에 고정으로 박혀있던 인터페이스 부분을 삭제

--- src/components/FetchData/fetchData.ts
+++ src/components/FetchData/fetchData.ts
@@ -5,10 +5,10 @@ import { ProductDTO } from "@/shared/interface/Data.interface";
  * @param endPoint - 요청할 엔드포인트 (예: "/product?number=1")
  * @returns Promise<ProductDTO[]> - 비동기 응답 (배열)
  */
-export default async function fetchData(
+export default async function fetchData<T>(
   baseURL: string,
   endPoint: string
-): Promise<ProductDTO[]> {
+): Promise<T> {
   if (!baseURL) {
     throw new Error("BaseURL이 정의되지 않았습니다.");
   }
@@ -18,9 +18,8 @@ export default async function fetchData(
   const response = await fetch(requestURL);
 
   if (!response.ok) {
-    // 여기서 로깅하지 않고, 에러 정보만 throw
     throw new Error(`요청 실패: ${response.status} ${response.statusText}`);
   }
 
-  return response.json();
+  return response.json() as Promise<T>;
 }
