fde331d3d668f6915c2ebccc8fb09c7a8d94dafe
saccharin01 <chousik01@gmail.com>
2025-05-12 11:14:03 +0900

    feat | queryStringBuilderInterface.ts
    쿼리스트링 빌더 인터페이스에 selectedDate 속성 수정

--- src/components/queryStringBuilder/queryStringBuilderInterface.ts
+++ src/components/queryStringBuilder/queryStringBuilderInterface.ts
@@ -3,7 +3,7 @@ import { Direction, Type } from "@/shared/static.types";
 export interface queryStringBuilderInterface {
   chipId: string;
   type: Type;
-  baseDate: string;
+  selectedDate: string;
   selectedHour?: string;
   direction?: Direction
   count?: number;
