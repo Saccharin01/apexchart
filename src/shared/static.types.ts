55c41b35f561dba7a58f4fe72a306b55d23e4b82
saccharin01 <chousik01@gmail.com>
2025-05-18 20:30:10 +0900

    feat | static.type.ts
    타입 선언에 차트에서 사용되는 타입 추가

--- src/shared/static.types.ts
+++ src/shared/static.types.ts
@@ -1,2 +1,6 @@
+import { SensorResponseDTO, AggregatedDataDTO } from "./interface/Data.interface";
+
+
 export type Direction = "before" | "after";
-export type Type = "year" | "month" | "day" | "hour";
+export type Type = "year" | "month" | "week" | "day" | "hour";
+export type ChartData = SensorResponseDTO | AggregatedDataDTO[];
